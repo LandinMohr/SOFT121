e573e8f47a9d96b46ce7866e691b626dde29600b
4022s <4022s@users.noreply.local>
2025-11-17 09:52:09 -0800

    Adding latest changes to project

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -3,22 +3,23 @@ import Home from './components/Home';
 import Login from './components/Login';
 import About from './components/About';
 import Weather from './components/Weather';
+import Products from './components/Products';
 import RequireAuth from './components/RequireAuth';
 import { BrowserRouter, Link, Route, Routes } from 'react-router';
-import Products from './components/products';
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
         <Link to="/">Home</Link> | <Link to="/about">About</Link> |{' '}
-        <Link to="/login">Login</Link> | <Link to="/weather">Weather</Link>
-        | <Link to="/product">Products</Link>
+        <Link to="/login">Login</Link> | <Link to="/weather">Weather</Link> |{' '}
+        <Link to="/products">Products</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
         <Route
           path="/weather"
           element={
@@ -27,7 +28,10 @@ function App() {
             </RequireAuth>
           }
         />
-        <Route path="/product" element={<Products />} />
+        <Route
+          path="/product/:productId"
+          element={<div>Product Details Page</div>}
+        />
       </Routes>
     </BrowserRouter>
   );
--- client-app/src/Components/Products.js
+++ client-app/src/Components/Products.js
@@ -1,58 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router';
 
 export default function Products() {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            try {
-                const response = await fetch("http://localhost:5156/products");
-                
-                
-                if (!response.ok) {
-                    throw new Error();
-                }
-                const data = await response.json();
-                
-                setProducts(data);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchProducts();
-    }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:5156/product');
 
-    return (
-        <div>
-            <h2>Products</h2>
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: {error.message}</p>}
-            {!loading && !error && (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Temperature (°C)</th>
-                            <th>Temperature (°F)</th>
-                            <th>Summary</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products.map((product) => (
-                            <tr key={product.id}>
-                                <td><Link to={`/products/${product.id}`}>{product.id}</Link></td>
-                                <td>{product.name}</td>
-                                <td>{product.price}</td>
-                                <td>{product.description}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            )}
-        </div>
-    );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+
+        setProducts(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Products</h2>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {!loading && !error && (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.productId}>
+                <td>
+                  <Link to={`/product/${product.productId}`}>
+                    {product.productName}
+                  </Link>
+                </td>
+                <td>{product.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
