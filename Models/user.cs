0feb94de188ca9b8b49485c648ec7ec63df31314
4022s <4022s@users.noreply.local>
2025-11-26 14:01:42 -0800

    added passwordHash

--- Models/user.cs
+++ Models/user.cs
@@ -4,7 +4,7 @@ public class User
 {
     public int Id { get; set; } = 0;
     public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
 }
